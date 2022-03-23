08de4d0fa2549ca6f71772d432e3e010972bb77d
skrina1217 <skrina1217@gmail.com>
2022-03-23 19:35:51 +0900

    Initial Commit

---
+++ LoAssistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoAssistant
+//
+//  Created by 김승헌 on 2022/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
