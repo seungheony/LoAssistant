ca9e99cd83a8b081fae683b5ade2b41dc20635d3
skrina1217 <skrina1217@gmail.com>
2022-03-25 21:44:27 +0900

    main menu

---
+++ LoAssistant/MainTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTableViewController.swift
+//  LoAssistant
+//
+//  Created by 김승헌 on 2022/03/25.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
